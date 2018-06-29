96103de3e7c8b182c51cebf0050b381290e55673
Fontkodo <alexgriffinsrq@gmail.com>
2018-06-29 12:02:09 -0400

    Added threads

--- src/gameserver/GameServer.java
+++ src/gameserver/GameServer.java
@@ -12,18 +12,37 @@ public class GameServer {
 
 	private static int connectionCounter;
 
-	public static void conversation(Socket socket) throws IOException, ParseException {
-		BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
-		while (true) {
-			String content = NetString.readString(bis);
-			System.out.println("Received: " + content);
-			JSONParser p = new JSONParser();
-			JSONObject ob = (JSONObject) p.parse(content);
-			ob.put("connection", connectionCounter++);
-			byte[] tempBytes = NetString.toNetStringBytes(ob.toJSONString());
-			socket.getOutputStream().write(tempBytes);
-			socket.getOutputStream().flush();
-			System.out.println(ob.toJSONString());
+	static class Conversation implements Runnable {
+
+		Socket socket;
+
+		Conversation(Socket socket) {
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+				while (true) {
+					String content = NetString.readString(bis);
+					System.out.println("Received: " + content);
+					JSONParser p = new JSONParser();
+					JSONObject ob = (JSONObject) p.parse(content);
+					ob.put("connection", connectionCounter++);
+					byte[] tempBytes = NetString.toNetStringBytes(ob.toJSONString());
+					socket.getOutputStream().write(tempBytes);
+					socket.getOutputStream().flush();
+					System.out.println(ob.toJSONString());
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+
+					}
+				}
+			} catch (Exception e) {
+
+			}
 		}
 	}
 
@@ -31,13 +50,9 @@ public class GameServer {
 		ServerSocket serverSocket = new ServerSocket(9999);
 		while (true) {
 			Socket s = serverSocket.accept();
-			System.out.println(serverSocket);
-			try {
-				conversation(s);
-				//s.close();
-			} catch (Exception e) {
-				System.err.println(e);
-			}
+			Runnable r = new Conversation(s);
+			Thread t = new Thread(r);
+			t.start();
 		}
 
 	}
