60d01077f81a36e13dc50db8b5342a1fc3495f31
Fontkodo <alexgriffinsrq@gmail.com>
2018-06-29 11:28:11 -0400

    Long-running conversation

--- src/gameserver/GameServer.java
+++ src/gameserver/GameServer.java
@@ -9,31 +9,35 @@ import org.json.simple.parser.*;
 import fontkodo.netstring.*;
 
 public class GameServer {
-	
+
 	private static int connectionCounter;
-	
+
 	public static void conversation(Socket socket) throws IOException, ParseException {
-		OutputStream os = socket.getOutputStream();
-		BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		char [] stuff = new char[100_000];
-		int n = br.read(stuff);
-		CharArrayReader car = new CharArrayReader(stuff, 0, n);
-		JSONParser p = new JSONParser();
-		JSONObject ob = (JSONObject) p.parse(car);
-		ob.put("connection", connectionCounter++);
-		byte[] tempBytes = NetString.toNetStringBytes(ob.toJSONString());
-		socket.getOutputStream().write(tempBytes);
-		socket.getOutputStream().flush();
-		System.out.println(ob.toJSONString());
+		BufferedInputStream bis = new BufferedInputStream(socket.getInputStream());
+		while (true) {
+			String content = NetString.readString(bis);
+			System.out.println("Received: " + content);
+			JSONParser p = new JSONParser();
+			JSONObject ob = (JSONObject) p.parse(content);
+			ob.put("connection", connectionCounter++);
+			byte[] tempBytes = NetString.toNetStringBytes(ob.toJSONString());
+			socket.getOutputStream().write(tempBytes);
+			socket.getOutputStream().flush();
+			System.out.println(ob.toJSONString());
+		}
 	}
 
 	public static void main(String[] args) throws IOException, ParseException {
 		ServerSocket serverSocket = new ServerSocket(9999);
-		while(true) {
+		while (true) {
 			Socket s = serverSocket.accept();
 			System.out.println(serverSocket);
-			conversation(s);
-			s.close();
+			try {
+				conversation(s);
+				//s.close();
+			} catch (Exception e) {
+				System.err.println(e);
+			}
 		}
 
 	}
